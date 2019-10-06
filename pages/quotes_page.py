b02eb2d79f1081825cc75e7ab94d45e2f792d5c9
Heather Gray <heathergray153@gmail.com>
2019-10-06 10:57:26 -0600

    QuoteParser class complete

--- pages/quotes_page.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-quotes_page = requests.get('http://quotes.toscrape.com/')
-print(quotes_page)
---
+++ parsers/quote.py
@@ -0,0 +1,31 @@
+from locators.quote_locators import QuoteLocators
+
+
+class QuoteParser:
+    '''
+    Find data about quote (content, author, tags)
+    '''
+
+    def __init__(self, parent):
+        self.parent = parent
+    
+    def __repr__(self):
+        return f'<Quote: {self.content} by {self.author}>'
+    
+    def __str__(self):
+        return f'Quote: "{self.content}"\n by {self.author}'
+
+    @property
+    def content(self):
+        locator = QuoteLocators.CONTENT
+        return self.parent.select_one(locator).string
+
+    @property
+    def author(self):
+        locator = QuoteLocators.AUTHOR
+        return self.parent.select_one(locator).string
+
+    @property
+    def tags(self):
+        locator = QuoteLocators.TAGS
+        return self.parent.select(locator)
